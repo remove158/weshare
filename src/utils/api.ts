27f2a40193f42ed6c9c3d04f630ddd925c2d04c8
Piyaphat Pinyo <remove158@gmail.com>
2022-08-09 00:08:21 +0700

    :sparkles: feat: api with types

---
+++ src/utils/api.ts
@@ -0,0 +1,96 @@
+import {
+	addDoc,
+	updateDoc,
+	collection,
+	doc,
+	serverTimestamp,
+	Timestamp,
+	QueryDocumentSnapshot,
+} from "firebase/firestore";
+import { db } from "@utils/firebase";
+import { useDocumentData } from "react-firebase-hooks/firestore";
+import { SetStateAction } from "react";
+import { Dispatch } from "react";
+const BILL_COLLECTION_NAME = "bills";
+const BILL_COLLECTION = collection(db, BILL_COLLECTION_NAME);
+
+const updateBillFunc = updateDoc<Bill>
+
+const generateDefaultBill = () => {
+	const time = serverTimestamp();
+	return { createdAt: time, updatedAt: time } as Bill;
+};
+
+const getBillRef = (id: string) =>
+	doc(db, BILL_COLLECTION_NAME, id).withConverter(converter);
+
+interface CreateBillArgs {
+	setLoading: Dispatch<SetStateAction<boolean>>;
+}
+export const createBill = async ({ setLoading }: CreateBillArgs) => {
+	setLoading(true);
+	const { id } = await addDoc(BILL_COLLECTION, generateDefaultBill());
+	return id;
+};
+
+interface PaidUser {
+	id: string;
+	status: string;
+}
+interface Order {
+	id: string;
+	name: string;
+	price: number;
+	paidUsers: PaidUser[];
+}
+
+interface User {
+	id: string;
+	name: string;
+}
+
+export interface Bill {
+	orders: Order[];
+	users: User[];
+	updatedAt: Timestamp;
+	createdAt: Timestamp;
+}
+
+const converter = {
+	toFirestore: (data: Bill) => data,
+	fromFirestore: (snap: QueryDocumentSnapshot) => snap.data() as Bill,
+};
+
+export const useBillQuery = (id: string) => {
+	const billRef = getBillRef(id);
+	return useDocumentData<Bill>(billRef);
+};
+
+export const updateOrders = async (id: string, orders: Order[]) => {
+	const billRef = getBillRef(id);
+	await updateBillFunc(billRef, {
+		orders,
+		updatedAt: serverTimestamp(),
+	});
+};
+
+export const updateUsers = async (id: string, users: User[]) => {
+	const billRef = getBillRef(id);
+	await updateBillFunc(billRef, {
+		users,
+		updatedAt: serverTimestamp(),
+	});
+};
+
+export const updateBill = async (
+	id: string,
+	orders: Order[],
+	users: User[]
+) => {
+	const billRef = getBillRef(id);
+	await updateBillFunc(billRef, {
+		users,
+		orders,
+		updatedAt: serverTimestamp(),
+	});
+};
---
+++ src/utils/firebase.ts
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+import { getFirestore } from "firebase/firestore";
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: "AIzaSyAbKa3tI0MQ86RwrVZUj3Ti7YHmcQS3HiU",
+	authDomain: "weshare-2f750.firebaseapp.com",
+	projectId: "weshare-2f750",
+	storageBucket: "weshare-2f750.appspot.com",
+	messagingSenderId: "26806537971",
+	appId: "1:26806537971:web:5319857c5b57c82e693186",
+	measurementId: "G-0DEK1S8BM3",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
