bd379ba594f45db88f31fbe7e59a0705e529107d
Piyaphat Pinyo <remove158@gmail.com>
2022-08-09 00:10:43 +0700

    :necktie: add: meta

---
+++ src/components/MetaTags.tsx
@@ -0,0 +1,26 @@
+import Head from "next/head";
+import React from "react";
+
+//-------------------------------------------------------------------------//
+// summary :  component types section
+//-------------------------------------------------------------------------//
+interface Props {}
+
+//-------------------------------------------------------------------------//
+// summary : component function section
+//-------------------------------------------------------------------------//
+const MetaTags: React.FC<Props> = (props) => {
+	return (
+		<Head>
+			<title>Share a Bill</title>
+			<meta
+				name="description"
+				content="หารบิลกับเพื่อน เราช่วยให้คุณสามารถจัดแจงค่าใช้จ่ายได้อย่างง่ายดาย"
+			/>
+			<meta name="theme-color" content="#2196f3" />
+			<link rel="icon" href="/favicon.ico" />
+		</Head>
+	);
+};
+
+export default MetaTags;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { ReactElement, ReactNode } from "react";
 import type { NextPage } from "next";
 import type { AppProps } from "next/app";
+import MetaTags from "@components/MetaTags";
 
 export type NextPageWithLayout = NextPage & {
 	getLayout?: (page: ReactElement) => ReactNode;
@@ -14,5 +15,10 @@ export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
 	// Use the layout defined at the page level, if available
 	const getLayout = Component.getLayout ?? ((page) => page);
 
-	return getLayout(<Component {...pageProps} />);
+	return (
+		<>
+			<MetaTags />
+			{getLayout(<Component {...pageProps} />)}
+		</>
+	);
 }
