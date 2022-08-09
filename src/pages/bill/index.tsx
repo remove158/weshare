22bd9ed08d71725ac1d5b6692622b9fd033e2308
Piyaphat Pinyo <remove158@gmail.com>
2022-08-09 13:59:31 +0700

    :bug: fix: import

--- src/pages/bill/index.tsx
+++ src/pages/bill/index.tsx
@@ -1,22 +1,6 @@
-import AlertCopy from "@components/AlertCopy";
 import BackDrop from "@components/Backdrop";
-import BillHeader from "@components/BillHeader";
 import BillInfo from "@components/BillInfo";
-import BillStats from "@components/BillStats";
-import Orders from "@components/Orders";
-import Users from "@components/Users";
-import HomeIcon from "@mui/icons-material/Home";
-import PeopleAltIcon from "@mui/icons-material/PeopleAlt";
-import SegmentIcon from "@mui/icons-material/Segment";
-import { Box, Container, Grid, Hidden, Paper, Snackbar } from "@mui/material";
-import BottomNavigation from "@mui/material/BottomNavigation";
-import BottomNavigationAction from "@mui/material/BottomNavigationAction";
-import { useBillQuery } from "@utils/api";
-import { getBillFullPath } from "@utils/route";
-import { addRecentBills } from "@utils/storage";
-import { GetServerSideProps } from "next";
 import { useRouter } from "next/router";
-import { ParsedUrlQuery } from "querystring";
 import React, { useEffect, useState } from "react";
 
 //-------------------------------------------------------------------------//
