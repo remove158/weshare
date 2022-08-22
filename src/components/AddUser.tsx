c2c6bfd1e8d86436b41363e1ca038a719f58b836
Piyaphat Pinyo <remove158@gmail.com>
2022-08-22 20:33:34 +0700

    fix: reuqire name

--- src/components/AddUser.tsx
+++ src/components/AddUser.tsx
@@ -1,9 +1,9 @@
 import PersonAddIcon from "@mui/icons-material/PersonAdd";
 import { Button, Grid, TextField } from "@mui/material";
 import { useState } from "react";
-import { v4 as uuidv4 } from "uuid";
-import { updateUsers } from "utils/api";
 import { Bill, User } from "types";
+import { updateUsers } from "utils/api";
+import { v4 as uuidv4 } from "uuid";
 
 const addPeople = (peoples: User[], name: string, id: string) => {
 	const item_id = uuidv4();
@@ -23,8 +23,10 @@ const AddPeople = ({ id, bill }: { id: string; bill: Bill }) => {
 			<form
 				onSubmit={(e) => {
 					e.preventDefault();
-					addPeople(users, text, id);
-					setText("");
+					if (text) {
+						addPeople(users, text, id);
+						setText("");
+					}
 				}}
 			>
 				<Grid container spacing={2}>
@@ -35,6 +37,7 @@ const AddPeople = ({ id, bill }: { id: string; bill: Bill }) => {
 							fullWidth
 							variant="standard"
 							onChange={(e) => setText(e.target.value)}
+							required
 						/>
 					</Grid>
 					<Grid item>
