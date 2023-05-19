80f50074e43fcf21e7643fb096f7e139f30987dc
Piyaphat Pinyo <remove158@gmail.com>
2023-05-18 23:37:53 -0400

    add: reset bill func

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -86,6 +86,15 @@ export const updateBill = async (
 	});
 };
 
+export const resetBill = async (id: string) => {
+	const billRef = getBillRef(id);
+	await updateBillFunc(billRef, {
+		users: [],
+		orders: [],
+		updatedAt: serverTimestamp(),
+	});
+}
+
 export const uploadFirebase = async (file: File, id: string) => {
 	const storageRef = ref(storage, `bills/${id}`);
 	// 'file' comes from the Blob or File API
